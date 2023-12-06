7baead3fa329517188a5443528b8bbb7ea19c326
アルゴリズムラボ <algorithmlab@arugorizumurabonoMac-mini.local>
2023-12-06 22:33:11 +0900

    a

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/k646592/golang_backend_labmaid
+
+go 1.21.5
--- mail.go
+++ mail.go
@@ -2,62 +2,20 @@ package main
 
 import (
 	"fmt"
-	"net/smtp"
+	"log"
 	"net/http"
 )
 
 func sendMail(w http.ResponseWriter, r *http.Request) {
-	name := r.FormValue("name")
-	subject := r.FormValue("subject")
-	fromEmail := r.FormValue("from")
-	text := r.FormValue("text")
-
-	fmt.Println(name)
-
-	smtpServer := "158.217.174.41"
-	smtpPort := 25
-	toEmail := "k646592@kansai-u.ac.jp"
-
-	body := "From: " + name + "<" + fromEmail + ">\r\n" + "To: " + toEmail + "\r\n" + "Subject: " + subject + "\r\n\r\n" + text
-
-	auth := smtp.PlainAuth("", "", "", smtpServer)
-
-	tlsConfig := &tls.Config(
-		InsecureSkipVerify: true,
-		ServerName: smtpServer,
-	)
-
-	conn, err := tls.Dial("top", fmt.Sprintf("%s:%d", smtpServer, smtpPort), tlsConfig)
-	err := smtp.SendMail(fmt.Sprintf("%s:%d", smtpServer, smtpPort), auth, fromEmail, []string{toEmail}, []byte(body))
+	hello := []byte("Hello World!!!")
+	_, err := w.Write(hello)
 	if err != nil {
-		fmt.Println("Error sending mail:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		log.Fatal(err)
 	}
-	defer conn.Close()
-
-	client, err := smtp.NewClient(conn, smtpServer)
-	if err != nil {
-		fmt.Println("Error creating SMTP client:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	defer client.Close()
-
-	
-
-	fmt.Println("Mail sent Successfully")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "Data received successfully"}`))
 }
 
 func main() {
-	http.HandleFunc("/gomail", sendMail)
-
-	fmt.Println("Server is running on :5000...")
-	err := http.ListenAndServe(":5000", nil)
-	if err != nil {
-		fmt.Println("Error starting server", err)
-	}
+	http.HandleFunc("/hello", sendMail)
+	fmt.Println("Server Start Up........")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
